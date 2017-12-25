3c30d47ce60a81e4bb7b7a8b4c8c95b8e10a9f08
Bonnie S <bonnie_sterling@hotmail.com>
2017-12-25 13:44:26 -0500

    Add files via upload

---
+++ CalculatorApp.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CalculatorApp : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        Calculator_Service.Calculator_WebServiceSoapClient client = new Calculator_Service.Calculator_WebServiceSoapClient();
+        int result = client.Add(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+        lblResult.Text = result.ToString();
+    }
+}
