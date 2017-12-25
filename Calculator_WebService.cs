7dd7b6616574d07a3dd7032b18da2125785c4616
Bonnie S <bonnie_sterling@hotmail.com>
2017-12-25 14:57:40 -0500

    Add files via upload
    Added Method GetCalcList

--- Calculator_WebService.cs
+++ Calculator_WebService.cs
@@ -21,10 +21,44 @@ public class Calculator_WebService : System.Web.Services.WebService
         //InitializeComponent(); 
     }
 
-    [WebMethod]
+    [WebMethod (EnableSession =true)]
     public int Add(int firstNumber, int secondNumber)
     {
+
+        //Intent is to save the history of webservice use
+        List<string> calculations;
+        //upon initial page load create the list holding history
+        if(Session["CALCULATIONS"] == null)
+        {
+            calculations = new List<string>();
+        }
+        else
+        {
+            //add current calculation to user history and output to gridview
+            calculations = (List<string>)Session["CALCULATIONS"];
+        }
+
+        string strRecentCalculation = firstNumber.ToString() + " + " + secondNumber.ToString() + " = " + (firstNumber + secondNumber).ToString();
+        calculations.Add(strRecentCalculation);
+        Session["CALCULATIONS"] = calculations;
         return firstNumber + secondNumber;
+    }
+
+    //return the list of calculations to the client
+    [WebMethod(EnableSession =true)]
+    public List<string> GetCalcList()
+    {
+        if(Session["CALCULATIONS"] == null)
+        {
+            List<string> calculations = new List<string>();
+            calculations.Add("You have not done any calculations yet");
+            return calculations;
+        }
+        else
+        {
+           return (List<string>) Session["CALCULATIONS"];
+        }
+
     }
 
 }
